d68d854b30f206bad0282ff1e417f150901e6d60
AJ Javadi <amirjavadi25@gmail.com>
2024-04-03 21:55:01 -0500

    exceptions

---
+++ Course Content/activities/exceptions.py
@@ -0,0 +1,11 @@
+# exceptions for error handling
+print("Errors: Try/Except")
+
+# try: 
+#     # code you want to run
+# except:
+#     # executed if error occurs
+# else:
+#     # executed if no error
+# finally:
+#     # always executed
